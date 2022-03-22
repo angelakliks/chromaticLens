d28cd917245119e291ab05a527757b72a5b43293
angelakliks <Angela.kliks@gmail.com>
2022-03-22 14:36:10 -0400

    Initial commit

---
+++ app/src/main/java/com/example/chromaticlens/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.chromaticlens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    //called when the user taps the "get satarted" button
+    public void onClick(View view) {
+        //do something
+    }
+}
