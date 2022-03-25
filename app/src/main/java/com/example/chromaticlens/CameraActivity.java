05e9570133c25c70dbcff67e33754e82e020d187
Miriam <mfarag3450@gmail.com>
2022-03-25 12:49:46 -0400

    updated 3.25 12:50pm

--- app/src/main/java/com/example/chromaticlens/CameraActivity.java
+++ app/src/main/java/com/example/chromaticlens/CameraActivity.java
@@ -3,27 +3,53 @@ package com.example.chromaticlens;
 import static com.example.chromaticlens.Camera.getCameraInstance;
 
 import android.app.Activity;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.view.SurfaceView;
 import android.widget.FrameLayout;
 
 public class CameraActivity extends Activity {
 
-        private Camera mCamera;
-        private Camera.CameraPreview mPreview;
+    private Camera mCamera;
+    private CameraPreview mPreview;
+    private SurfaceView preview;
+    private MediaRecorder mediaRecorder;
 
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.main);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.camera);
 
-            // Create an instance of Camera
-            mCamera = getCameraInstance();
+        // Create an instance of Camera
+        mCamera = getCameraInstance();
 
-            // Create our Preview view and set it as the content of our activity.
-            mPreview = new CameraPreview(this, mCamera);
-            FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
-            preview.addView(mPreview);
+        // Create our Preview view and set it as the content of our activity.
+        mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
 
     }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        releaseMediaRecorder();       // if you are using MediaRecorder, release it first
+        releaseCamera();              // release the camera immediately on pause event
+    }
 
+    private void releaseMediaRecorder(){
+        if (mediaRecorder != null) {
+            mediaRecorder.reset();   // clear recorder configuration
+            mediaRecorder.release(); // release the recorder object
+            mediaRecorder = null;
+        }
+    }
+
+    private void releaseCamera(){
+        if (mCamera != null){
+            mCamera.release();        // release the camera for other applications
+            mCamera = null;
+        }
+    }
 }
+
+
